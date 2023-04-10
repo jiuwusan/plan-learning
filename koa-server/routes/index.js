957af0457faf5094f913f361908928b75436f38d
jiuwusan <zkdhzr@gmail.com>
2023-04-10 20:20:25 +0800

    delete server

--- koa-server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router =  require('koa-router')();
-const user = require('../contoller/user');
-const question = require('../contoller/question');
-
-router.use('/user', user.routes(), user.allowedMethods());
-router.use('/question', question.routes(), question.allowedMethods());
-
-module.exports = router;
