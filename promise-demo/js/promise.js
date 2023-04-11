118c1ad1b04046a69a2b9d73de553c8af6cfa524
ZhouKaiDong <953343906@qq.com>
2023-04-11 18:04:22 +0800

    promise day2-1

--- promise-demo/js/promise.js
+++ promise-demo/js/promise.js
@@ -51,19 +51,47 @@ function Promise(excutor) {
  * @returns 
  */
 Promise.prototype.then = function (onResolved, onRejected) {
+    let _this = this;
+    if (typeof onResolved !== 'function') onResolved = (value) => value
+    if (typeof onRejected !== 'function') onRejected = (error) => error
     // 通过 promiseState 状态 执行 回调函数
     return new Promise((resolve, reject) => {
-        switch (this.promiseState) {
+
+        function callback(fn) {
+            try {
+                let result = fn(_this.promiseResult);
+                if (result instanceof Promise) {
+                    result.then((v) => {
+                        resolve(v)
+                    }, (r) => {
+                        reject(r)
+                    })
+                } else {
+                    resolve(result)
+                }
+            } catch (error) {
+                reject(error)
+            }
+        }
+
+        switch (_this.promiseState) {
             case 'pending':// 等待
                 // 保存回调函数
-                this.callbacks.push({
-                    onResolved,
-                    onRejected
+                _this.callbacks.push({
+                    onResolved: () => {
+                        callback(onResolved)
+                    },
+                    onRejected: () => {
+                        callback(onRejected)
+                    },
                 })
                 break
             case 'fulfilled':// 成功
+                callback(onResolved)
+                break
+            case 'rejected': //失败
                 try {
-                    let result = onResolved(this.promiseResult);
+                    let result = onRejected(_this.promiseResult);
                     if (result instanceof Promise) {
                         result.then((v) => {
                             resolve(v)
@@ -77,9 +105,6 @@ Promise.prototype.then = function (onResolved, onRejected) {
                     reject(error)
                 }
                 break
-            case 'rejected': //失败
-                let result = onRejected(this.promiseResult)
-                break
         }
     })
 }
@@ -87,9 +112,87 @@ Promise.prototype.then = function (onResolved, onRejected) {
 /**
  * 异常处理
  * 
- * @param {*} onResolved 
  * @param {*} onRejected 
  */
-Promise.prototype.catch = function (onResolved, onRejected) {
+Promise.prototype.catch = function (onRejected) {
 
-    }
+}
+
+/**
+ * 添加成功方法
+ * 
+ * @param {*} value 
+ * @returns 
+ */
+Promise.resolve = function (value) {
+    return new Promise((resolve, reject) => {
+        if (value instanceof Promise) {
+            value.then(resolve, reject)
+        } else {
+            resolve(value)
+        }
+    })
+}
+
+/**
+ * 添加 失败方法
+ * 
+ * @param {*} reason 
+ * @returns 
+ */
+Promise.reject = function (reason) {
+    return new Promise((resolve, reject) => {
+        reject(reason)
+    })
+}
+
+/**
+ * 添加 all 方法
+ * 所有promise执行成功
+ */
+Promise.all = function (promiseArray) {
+
+    return new Promise((resolve, reject) => {
+        let count = 0;
+        let result = [];
+        // 遍历
+        for (let i = 0; i < promiseArray.length; i++) {
+            promiseArray[i].then((value) => {
+                count++;
+                result[i] = value;
+                if (count === promiseArray.length) {
+                    // resolve(promiseArray.map(() => promiseArray.promiseResult))
+                    resolve(result);
+                }
+            }, (error) => {
+                reject(error)
+            })
+
+        }
+    })
+}
+
+/**
+ * 一旦有成功，就 resolve
+ * 
+ * @param {*} promiseArray 
+ * @returns 
+ */
+Promise.race = function (promiseArray) {
+
+    return new Promise((resolve, reject) => {
+
+        // 遍历
+        for (let i = 0; i < promiseArray.length; i++) {
+            promiseArray[i].then((value) => {
+                if (count === promiseArray.length) {
+                    // resolve(promiseArray.map(() => promiseArray.promiseResult))
+                    resolve(result);
+                }
+            }, (error) => {
+                reject(error)
+            })
+
+        }
+    })
+}
